2fbb38413b4b7111160b96e9f70e8c01a1080980
C-32-Rutujam <103025945+C-32-Rutujam@users.noreply.github.com>
2022-04-05 12:59:52 +0530

    Add files via upload

---
+++ P1_Talk to a teacher.c
@@ -0,0 +1,24 @@
+//My First C program
+
+#include<stdio.h>
+
+int main()
+
+{
+    
+ printf("Talk to a teacher\n");
+   
+ printf("Welcome\n");
+    
+ return 0;
+
+}
+
+
+OUTPUT:
+Talk to a teacher
+Welcome
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
